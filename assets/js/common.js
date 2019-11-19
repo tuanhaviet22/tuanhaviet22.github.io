f06ddcddb97091c0892417ffe25087d7cadef0ad
Tuan <tuanhaviet22@gmail.com>
2019-11-19 18:05:12 +0700

    Tin tức

--- assets/js/common.js
+++ assets/js/common.js
@@ -36,4 +36,24 @@ $(document).ready(function(){
             }
         }
     })
+
+    $('.customer-rv').owlCarousel({
+        items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 })
