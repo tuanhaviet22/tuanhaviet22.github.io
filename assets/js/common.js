c09dddce65e7da67d9f9f852389c80a8a14c5598
Tuan <tuanhaviet22@gmail.com>
2019-11-25 20:04:55 +0700

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,62 +1,104 @@
 $(document).ready(function () {
     $('.show-popup').trigger('click')
     $('.fst-i').trigger('click')
-    $('.slider-banner').owlCarousel({
+    // $('.slider-banner').owlCarousel({
+    //     loop: true,
+    //     nav: true,
+    //     dots: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         600: {
+    //             items: 1
+    //         },
+    //         1000: {
+    //             items: 1
+    //         }
+    //     }
+    // })
+    var swiper = new Swiper('.slider-banner', {
+        // navigation: {
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        // },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            // type: 'fraction',
+        },
         loop: true,
-        nav: true,
-        dot: false,
-        responsive: {
-            0: {
-                items: 1
+        autoplay: {
+            delay: 3000,
+        },
+        slidesPerView: 1,
+        //   freeMode : true
+    });
+    var swiper2 = new Swiper(".start-review", {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="dot-cus ' + className + '">' + '</span>';
             },
-            600: {
-                items: 1
+        },
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+        slidesPerView: 3,
+        spaceBetween: 20,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
             },
-            1000: {
-                items: 1
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 40
             }
         }
-    })
-
-    $('.start-review').owlCarousel({
-        items: 4,
+    });
+    var swiper3 = new Swiper('.customer-rv',{
+        slidesPerView: 1,
         loop: true,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 1000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
+        autoplay: {
+            delay: 3000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="dot-cus ' + className + '">' + '</span>';
             },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            }
-        }
+        },
     })
-
-    $('.customer-rv').owlCarousel({
-        items: 4,
-        loop: true,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 1000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    })
+    // $('.customer-rv').owlCarousel({
+    //     items: 4,
+    //     loop: true,
+    //     margin: 10,
+    //     autoplay: true,
+    //     autoplayTimeout: 1000,
+    //     autoplayHoverPause: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         600: {
+    //             items: 1
+    //         },
+    //         1000: {
+    //             items: 1
+    //         }
+    //     }
+    // })
 })
 $('.up').click(function (e) {
     // e.preventDefault();
@@ -96,9 +138,9 @@ $(window).scroll(function () {
 })
 
 
-$('.btn-show-menu').click(function(){
-    $('#navbar').css('left','0')
+$('.btn-show-menu').click(function () {
+    $('#navbar').css('left', '0')
 })
-$('.icon-close').click(function(){
-    $('#navbar').css('left','-20rem')
+$('.icon-close').click(function () {
+    $('#navbar').css('left', '-20rem')
 })
